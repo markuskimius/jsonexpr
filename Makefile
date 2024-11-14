07ef3198eb48d6dea354938e642a10c2f81db8fb
Mark Kim <markuskimius@gmail.com>
2024-11-13 23:28:54 -0500

    remove debug message, test script, version 0.0.21

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-JE_VERSION:=0.0.21
+JE_VERSION:=0.0.22
 SHDOC:=src/je.h src/main.c js/lib/je.js py/bin/je py/setup.py py/lib/je.py py/lib/__init__.py
 
 .PHONY: all clean install uninstall preprocess
---
+++ test/regression.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in *.je; do
+    printf "%-15s .. " "$file"
+
+    if diff -q <(../bin/je -q "$file" 2>&1) "out/$(basename "$file" .je).out" 2>/dev/null; then
+        printf "[OK]\n"
+    else
+        printf "[ERROR]\n"
+    fi
+done
