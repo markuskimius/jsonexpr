a592d62d211ac9da658c65b8ce3e168fe4860b3f
Mark Kim <markuskimius@gmail.com>
2024-11-17 12:11:03 -0500

    C convenience functions

---
+++ src/je.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include "je.h"
+#include "je_map.h"
+#include "je_error.h"
+
+
+/* ***************************************************************************
+* PUBLIC FUNCTIONS
+*/
+
+JE_COMPILED* je_compile(const char* code) {
+    JE_COMPILED* compiled = calloc(1, sizeof(JE_COMPILED));
+
+    compiled->ast = je_parse(code);
+    compiled->symtbl0 = je_newtable(NULL);
+    compiled->symtbl = je_newtable(compiled->symtbl0);
+
+    return compiled;
+}
+
+void je_free(JE_COMPILED* compiled) {
+    je_freenode(compiled->ast);
+    je_freetable(compiled->symtbl);
+    je_freetable(compiled->symtbl0);
+
+    compiled->ast = NULL;
+    compiled->symtbl = NULL;
+    compiled->symtbl0 = NULL;
+
+    free(compiled);
+}
+
+JE_VAL* je_evalc(JE_COMPILED* compiled) {
+    return je_eval(compiled->ast, compiled->symtbl);
+}
+
+int je_setSymbols(JE_COMPILED* compiled, const char* json) {
+    JE_COMPILED* c = je_compile(json);
+    JE_VAL* r = je_evalc(c);
+    int isok = 0;
+
+    if(r->type == JE_OBJECT_V) {
+        JE_MAP* m = r->value.m;
+
+        while((m = je_mapnext(m))) {
+            const char* key = je_mapkey(m);
+            JE_VAL* value = je_mapval(m);
+
+            je_tableset(compiled->symtbl, key, je_dupval(value), 1);
+        }
+
+        isok = 1;
+    }
+
+    je_freeval(r);
+    je_free(c);
+
+    return isok;
+}
+
+void je_clearSymbols(JE_COMPILED* compiled) {
+    je_tableclear(compiled->symtbl, 1);
+}
--- test/c/test.c
+++ test/c/test.c
@@ -1,28 +1,26 @@
 #include <stdlib.h>
-#include <sys/types.h>
 #include "je/je.h"
-#include "je/je_map.h"
-#include "je/je_symtbl.h"
-
-const char* code =
-    "PRINT(\"I have \" + LEN(grades) + \" students\");\n"
-    "PRINT(\"Alice's grade is \" + grades.alice);\n"
-;
 
 int main() {
-    JE_NODE* ast = je_parse(code);
-    JE_SYMTBL* symbols = je_newtable(NULL);
-    JE_MAP* grades = je_newmap();
     JE_VAL* result = NULL;
+    JE_COMPILED* compiled = je_compile(
+        "PRINT(\"I have \" + LEN(grades) + \" students\");"
+        "PRINT(\"Alice's grade is \" + grades.alice);"
+    );
 
-    je_mapset(grades, "alice", je_strval("A"));
-    je_mapset(grades, "bob", je_strval("B"));
-    je_tableset(symbols, "grades", je_objval(grades), 0);
+    je_setSymbols(compiled,
+        "{"
+        "  \"grades\" : {"
+        "    \"alice\" : \"A\","
+        "    \"bob\"   : \"B\","
+        "  }"
+        "}"
+    );
 
-    result = je_eval(ast, symbols);
+    result = je_evalc(compiled);
 
     je_freeval(result);
-    je_freenode(ast);
+    je_free(compiled);
 
     return 0;
 }
