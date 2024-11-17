b964a1151479d26c52e441adb87cbfd6d0156849
Mark Kim <markuskimius@gmail.com>
2024-11-16 21:35:32 -0500

    more tests

--- test/regression.sh
+++ test/regression.sh
@@ -3,7 +3,7 @@
 for file in *.je; do
     printf "%-15s .. " "$file"
 
-    if diff -q <(../bin/je -q "$file" 2>&1) "out/$(basename "$file" .je).out" 2>/dev/null; then
+    if diff -q <(../bin/je -q "$file" 2>&1) "out/$(basename "$file" .je).out" >&/dev/null; then
         printf "[OK]\n"
     else
         printf "[ERROR]\n"
