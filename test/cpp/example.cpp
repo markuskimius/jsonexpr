3de4b00310593886ca0bfa7e26c12c361a69b953
Mark Kim <markuskimius@gmail.com>
2025-06-23 01:04:48 -0400

    match C++ example

--- test/cpp/example.cpp
+++ test/cpp/example.cpp
@@ -6,7 +6,6 @@ int main() {
     auto parsed = instance.parse(R"je(
         PRINT("I have " + LEN(grades) + " students");
         PRINT("Alice's grade is " + grades.alice);
-        grades
     )je");
     auto symmap = instance.symmap(R"je({
         "grades" : {
